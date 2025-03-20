a08c56536d07ea78e717f606477fa131a298b86e
Mark Metcalfe <mark@markmetcalfe.com>
2025-03-20 21:02:48 +1300

    Adjust renderer resolution to be based on device pixel ratio

--- src/App.vue
+++ src/App.vue
@@ -104,10 +104,6 @@ button:focus:not(:disabled) {
   cursor: pointer;
 }
 
-canvas[data-engine^='three.js'] {
-  transform: scale(0.5) translateX(-50%) translateY(-50%);
-}
-
 .dark {
   color: var(--color-dark);
 }
