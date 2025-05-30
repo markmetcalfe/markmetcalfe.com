54ec0fd8e9b6c8dcbda48870c13c925801ff846b
Mark Metcalfe <mark@markmetcalfe.com>
2025-05-30 18:48:16 +1200

    Used dedicated styled list item component

---
+++ components/ListItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <li class="listitem">
+    <span class="listitem-inner">
+      <slot />
+    </span>
+  </li>
+</template>
+
+<style lang="scss">
+@use "~/variables" as vars;
+
+.listitem {
+  display: flex;
+
+  /* Workaround for Safari */
+  list-style-type: none;
+
+  &::marker {
+    display: none;
+  }
+
+  &::before {
+    content: "> ";
+    color: var(--color-highlight);
+    padding-right: 0.25rem;
+  }
+
+  &-inner {
+    & > *:first-child, & > p {
+      margin-top: 0;
+      padding-top: 0;
+    }
+  }
+}
+</style>
